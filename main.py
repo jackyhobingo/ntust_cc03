ed0d4f3ab0c98fab0bf9cc69320b925200d54ece
jackyhobingo <jackyhobingo@gmail.com>
2018-06-06 06:15:21 +0800

    Add config setting and fixed bug
    The bug which change the dictionary when query

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 from population import Population
 from village import Village
 from compute import list_add
+from flask import Flask, request
+from config import DevConfig
+import json
+
 p = Population()
 v = Village()
 
 
 def multiply_population_data(population_data, proportion):
-    population_data["household_no"] *= proportion
-    population_data["people_total"] *= proportion
-    population_data["people_total_m"] *= proportion
-    population_data["people_total_f"] *= proportion
-    population_data["people_m"] = [t*proportion for t in population_data["people_m"]]
-    population_data["people_f"] = [t*proportion for t in population_data["people_f"]]
-    return population_data
+    result = dict()
+    result["household_no"] = population_data["household_no"] * proportion
+    result["people_total"] = population_data["people_total"] * proportion
+    result["people_total_m"] = population_data["people_total_m"] * proportion
+    result["people_total_f"] = population_data["people_total_f"] * proportion
+    result["people_m"] = [t*proportion for t in population_data["people_m"]]
+    result["people_f"] = [t*proportion for t in population_data["people_f"]]
+    return result
 
 def add_population_data(population_data1, population_data2):
     if population_data1 == {}:
         return population_data2
     else:
-        population_data1["household_no"] += population_data2["household_no"]
-        population_data1["people_total"] += population_data2["people_total"]
-        population_data1["people_total_m"] += population_data2["people_total_m"]
-        population_data1["people_total_f"] += population_data2["people_total_f"]
-        population_data1["people_m"] = list_add(population_data1["people_m"], population_data2["people_m"])
-        population_data1["people_f"] = list_add(population_data1["people_f"], population_data2["people_f"])
-        return population_data1
+        result = dict()
+        result["household_no"] = population_data1["household_no"] + population_data2["household_no"]
+        result["people_total"] = population_data1["people_total"] + population_data2["people_total"]
+        result["people_total_m"] = population_data1["people_total_m"] + population_data2["people_total_m"]
+        result["people_total_f"] = population_data1["people_total_f"] + population_data2["people_total_f"]
+        result["people_m"] = list_add(population_data1["people_m"], population_data2["people_m"])
+        result["people_f"] = list_add(population_data1["people_f"], population_data2["people_f"])
+        return result
 
 def get_villages_name_and_proportion(latitude, longitude, radius):
     villages = v.find_cross_villages(latitude=latitude, longitude=longitude, radius=radius)
@@ -38,27 +44,54 @@ def get_villages_name_and_proportion(latitude, longitude, radius):
 def get_population(latitude, longitude, radius):
 
     villages = get_villages_name_and_proportion(latitude, longitude, radius)
-    print(villages)
-    population_data = {}
+    # print(villages)
+    population_data = dict()
     for n_p in villages:
         name = n_p[0]
         proportion = n_p[1]
         new_population_data = multiply_population_data(p.d[name], proportion)
         population_data = add_population_data(population_data, new_population_data)
 
-    carry(population_data)
-    return population_data
+
+    return carry(population_data)
 
 def carry(population_data):
+    result = dict()
+    result["people_total_m"] = int(0.5 + population_data["people_total_m"])
+    result["people_total_f"] = int(0.5 + population_data["people_total_f"])
+    result["household_no"] = int(population_data["household_no"] + 0.5)
+    result["people_total"] = int(0.5 + population_data["people_total"])
+
+    result["people_m"] = [int(t + 0.5) for t in population_data["people_m"]]
+    result["people_f"] = [int(t + 0.5) for t in population_data["people_f"]]
+    return result
 
 
-    population_data["people_total_m"] = int(0.5 + population_data["people_total_m"])
-    population_data["people_total_f"] = int(0.5 + population_data["people_total_f"])
-    population_data["household_no"] = int(population_data["household_no"] + 0.5)
-    population_data["people_total"] = int(0.5 + population_data["people_total"])
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+blank = {}
+@app.route("/")
+def hello():
+    try:
+        latitude = request.args.get("la")
+        longitude = request.args.get("lo")
+        radius = request.args.get("r")
+        d = get_population(float(latitude), float(longitude), float(radius))
+        # print(d)
+        return json.dumps(d)
+    except:
 
-    population_data["people_m"] = [int(t + 0.5) for t in population_data["people_m"]]
-    population_data["people_f"] = [int(t + 0.5) for t in population_data["people_f"]]
-    return population_data
+        return json.dumps(blank)
 
-print(get_population(24.976340, 121.280176, 500))
+if __name__ == "__main__":
+
+    blank["household_no"] = 0
+    blank["people_total"] = 0
+    blank["people_total_m"] = 0
+    blank["people_total_f"] = 0
+    blank["people_m"] = [0 for t in range(101)]
+    blank["people_f"] = [0 for t in range(101)]
+    app.run(host="0.0.0.0", port=8080)
+
+
+
